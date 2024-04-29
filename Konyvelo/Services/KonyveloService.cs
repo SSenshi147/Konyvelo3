2c85ed056166a4ef94168043b9b85fa7d700d787
SSenshi147 <47696835+SSenshi147@users.noreply.github.com>
2024-04-29 23:09:43 +0200

    Fix/insert and pivot (#1)
    insert navigation property fixed
    pivot updated with category spending

--- Konyvelo/Services/KonyveloService.cs
+++ Konyvelo/Services/KonyveloService.cs
@@ -2,46 +2,57 @@ using CsharpGoodies.Common.Extensions;
 using Konyvelo.Data;
 using Konyvelo.Domain;
 using Konyvelo.Dtos;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace Konyvelo.Services;
 
-public class KonyveloService(KonyveloDbContext context, IConfiguration configuration)
+public class KonyveloService(KonyveloDbContext context)
 {
-    public async Task<List<Transaction>> GetAllTransactions(CancellationToken cancellationToken = default)
+    public async Task<List<Transaction>> GetAllTransactions()
     {
         return await context
             .Transactions
             .Include(x => x.Account)
             .ThenInclude(x => x.Currency)
-            .ToListAsync(cancellationToken);
+            .ToListAsync();
     }
 
-    public async Task<DateOnly> GetFirstTransactionDate(CancellationToken cancellationToken = default)
+    public async Task<DateOnly> GetFirstTransactionDate()
     {
         var query = await context
             .Transactions
             .OrderBy(x => x.Date)
-            .FirstOrDefaultAsync(cancellationToken);
+            .FirstOrDefaultAsync();
 
         return query.Date;
     }
 
-    public async Task<PivotTransactionDto> GetPivotTransactions(DateOnly begindate, DateOnly endDate,
-        CancellationToken cancellationToken = default)
+    public async Task<PivotTransactionDto> GetPivotTransactions(DateOnly beginDate, DateOnly endDate
+        )
     {
-        var transactions = await context.Transactions.ToListAsync(cancellationToken);
+        var transactions = await context
+            .Transactions
+            .Include(x => x.Account)
+            .ThenInclude(x => x.Currency)
+            .Where(x => x.Date >= beginDate && x.Date <= endDate)
+            .ToListAsync();
 
         var categories = transactions
-            .GroupBy(x => x.Category)
-            .Select(x => new PivotTransaction
-            {
-                Category = x.Key,
-                Transactions = transactions.Where(y => y.Category == x.Key && y.Date.IsBetween(begindate, endDate))
-                    .ToList(),
-            })
-            .ToList();
+             .GroupBy(x => x.Category)
+             .Select(x => new PivotTransactionCategory()
+             {
+                 Category = x.Key,
+                 Transactions = x.GroupBy(y => y.Account.Currency).Select(y => new PivotTransactionCurrency()
+                 {
+                     CurrencyCode = y.Key.Code,
+                     Transactions = y.Select(z => new PivotTransactionInfo()
+                     {
+                         Date = z.Date,
+                         Info = z.Info ?? "N/A",
+                         Total = z.Total
+                     }).ToList()
+                 }).ToList()
+             }).ToList();
 
         var response = new PivotTransactionDto()
         {
@@ -51,133 +62,102 @@ public class KonyveloService(KonyveloDbContext context, IConfiguration configura
         return response;
     }
 
-    public async Task<List<Account>> GetAllWallets(CancellationToken cancellationToken = default)
+    public async Task<List<Account>> GetAllWallets()
     {
         return await context
             .Wallets
             .Include(x => x.Transactions)
             .Include(x => x.Currency)
             .OrderBy(x => x.Name)
-            .ToListAsync(cancellationToken);
+            .ToListAsync();
     }
 
-    public async Task<List<Currency>> GetAllCurrencies(CancellationToken cancellationToken = default)
+    public async Task<List<Currency>> GetAllCurrencies()
     {
-        return await context.Currencies.ToListAsync(cancellationToken);
+        return await context.Currencies.ToListAsync();
     }
 
-    public async Task CreateCurrency(Currency currency, CancellationToken cancellationToken = default)
+    public async Task CreateCurrency(Currency currency)
     {
-        await context.Currencies.AddAsync(currency, cancellationToken);
-        await context.SaveChangesAsync(cancellationToken);
+        await context.Currencies.AddAsync(currency);
+        await context.SaveChangesAsync();
     }
 
-    public async Task CreateWallet(Account account, CancellationToken cancellationToken = default)
+    public async Task CreateWallet(CreateAccountModel dto)
     {
-        await context.Wallets.AddAsync(account, cancellationToken);
-        await context.SaveChangesAsync(cancellationToken);
+        if (dto is null) throw new ArgumentNullException(nameof(dto));
+        if (dto.CurrencyId < 0) throw new ArgumentOutOfRangeException(nameof(dto.CurrencyId));
+        if (string.IsNullOrEmpty(dto.Name)) throw new ArgumentException(nameof(dto.Name));
+
+        var currency = await context.Currencies.FirstOrDefaultAsync(x => x.Id  == dto.CurrencyId) ?? throw new Exception("currency not found");
+        var account = new Account()
+        {
+            CurrencyId = dto.CurrencyId,
+            Currency = currency,
+            Name = dto.Name
+        };
+
+        await context.Wallets.AddAsync(account);
+        await context.SaveChangesAsync();
     }
 
-    public async Task UpdateCurrency(Currency currency, CancellationToken cancellationToken = default)
+    public async Task UpdateCurrency(Currency currency)
     {
         context.Currencies.Update(currency);
-        await context.SaveChangesAsync(cancellationToken);
+        await context.SaveChangesAsync();
     }
 
-    public async Task UpdateWallet(Account account, CancellationToken cancellationToken = default)
+    public async Task UpdateWallet(Account account)
     {
         context.Wallets.Update(account);
-        await context.SaveChangesAsync(cancellationToken);
+        await context.SaveChangesAsync();
     }
 
-    public async Task DeleteCurrency(Currency currency, CancellationToken cancellationToken = default)
+    public async Task DeleteCurrency(Currency currency)
     {
         context.Currencies.Remove(currency);
-        await context.SaveChangesAsync(cancellationToken);
+        await context.SaveChangesAsync();
     }
 
-    public async Task DeleteWallet(Account account, CancellationToken cancellationToken = default)
+    public async Task DeleteWallet(Account account)
     {
         context.Wallets.Remove(account);
-        await context.SaveChangesAsync(cancellationToken);
+        await context.SaveChangesAsync();
     }
 
-    public async Task CreateTransaction(CreateTransactionModel transaction, CancellationToken cancellationToken = default)
+    public async Task CreateTransaction(CreateTransactionModel dto)
     {
-        if (transaction is null) throw new ArgumentNullException(nameof(transaction));
-        if (transaction.AccountId < 1) throw new ArgumentOutOfRangeException(nameof(transaction.AccountId));
-        if (transaction.Category.IsNullOrEmpty()) throw new ArgumentException(nameof(transaction.Category));
-        
-        await using var connection = new SqliteConnection(configuration["SqliteConnectionString"]);
-        connection.Open();
-        await using var command = connection.CreateCommand();
-        command.CommandText = "insert into transactions (account_id, category, date, info, total) values (@AccountValue, @CategoryValue, @DateValue, @InfoValue, @TotalValue)";
+        if (dto is null) throw new ArgumentNullException(nameof(dto));
+        if (dto.AccountId < 1) throw new ArgumentOutOfRangeException(nameof(dto.AccountId));
+        if (dto.Category.IsNullOrEmpty()) throw new ArgumentException(nameof(dto.Category));
 
-        var account_id = command.CreateParameter();
-        account_id.ParameterName = "AccountValue";
-        account_id.Value = transaction.AccountId;
-        command.Parameters.Add(account_id);
+        var account = await context.Wallets.FirstOrDefaultAsync(x => x.Id == dto.AccountId) ?? throw new Exception("account not found");
+        var transaction = new Transaction()
+        {
+            Account = account,
+            AccountId = dto.AccountId,
+            Category = dto.Category,
+            Date = dto.Date,
+            Info = dto.Info,
+            Total = dto.Total
+        };
 
-        var category =  command.CreateParameter();
-        category.ParameterName = "CategoryValue";
-        category.Value = transaction.Category;
-        command.Parameters.Add(category);
-
-        var date = command.CreateParameter();
-        date.ParameterName = "DateValue";
-        date.Value = transaction.Date.ToString("yyyy-MM-dd");
-        command.Parameters.Add(date);
-
-        var info = command.CreateParameter();
-        info.ParameterName = "InfoValue";
-        info.Value = transaction.Info;
-        command.Parameters.Add(info);
-
-        var total  = command.CreateParameter();
-        total.ParameterName = "TotalValue";
-        total.Value = transaction.Total;
-        command.Parameters.Add(total);
-
-        await command.ExecuteNonQueryAsync(cancellationToken);
+        await context.Transactions.AddAsync(transaction);
+        await context.SaveChangesAsync();
     }
 
-    public async Task UpdateTransaction(Transaction transaction, CancellationToken cancellationToken = default)
+    public async Task UpdateTransaction(Transaction transaction)
     {
         context.Transactions.Update(transaction);
-        await context.SaveChangesAsync(cancellationToken);
+        await context.SaveChangesAsync();
     }
 
-    public async Task DeleteTransaction(Transaction transaction, CancellationToken cancellationToken = default)
+    public async Task DeleteTransaction(Transaction transaction)
     {
         context.Transactions.Remove(transaction);
-        await context.SaveChangesAsync(cancellationToken);
+        await context.SaveChangesAsync();
     }
-
-    // public async Task Import()
-    // {
-    //var path = this.configuration["ImportCsv"] ?? throw new Exception("asd");
-
-    //var lines = await File.ReadAllLinesAsync(path, cancellationToken);
-
-    //foreach (var line in lines)
-    //{
-    //    var split = line.Split(';');
-
-    //    var transaction = new Transaction
-    //    {
-    //        Date = DateTime.Parse(split[0]),
-    //        Category = split[1],
-    //        Name = split[2],
-    //        Type = (TransactionType)int.Parse(split[3]),
-    //        Total = decimal.Parse(split[4]),
-    //        WalletId = new Guid("367E547A-448E-42C2-BDCB-8ED6FAA55B1F"),
-    //    };
-
-    //    await crudRepo.AddAsync(transaction, cancellationToken);
-    //}
-
-    //await crudRepo.SaveChangesAsync(cancellationToken);
-    // }
 }
 
 public record CreateTransactionModel(int AccountId, string Category, decimal Total, DateOnly Date, string? Info);
+public record CreateAccountModel(int CurrencyId, string Name);
